abb255fbd143ad714cf94b70d3084565f0fdbd4a
imsahil35 <130961969+imsahil35@users.noreply.github.com>
2023-04-28 09:29:57 +0530

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -1,10 +1,10 @@
--- creating database and its name
--- create database RGM;
+creating database and its name
+create database RGM;
 
--- create table student(
--- student_id int primary key,
--- student_name varchar(30) not null,
--- student_contact int not null,
--- student_dept varchar(20) not null
--- );
+create table student(
+student_id int primary key,
+student_name varchar(30) not null,
+student_contact int not null,
+student_dept varchar(20) not null
+);
 select * from student;
