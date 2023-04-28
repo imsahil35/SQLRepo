c4fba90890b4a33a62789457bd8cea47d43a4107
imsahil35 <130961969+imsahil35@users.noreply.github.com>
2023-04-28 10:40:40 +0530

    Update testq.sql

--- testq.sql
+++ testq.sql
@@ -6,15 +6,15 @@ Session varchar(20),
 Contact_no int, 
 Address varchar(20)
 ); 
-insert into rgm values("sahil","rgm","ml",687900,"nandya");
-insert into rgm values("sahvhw","rgm","c",987900,"nandya");
-insert into rgm values("jbjl","rgm","pyhton",787900,"nandya");
-insert into rgm values("sahwfyl","rgm","c++",487900,"nandya");
-insert into rgm values("fuewil","rgm","c",187900,"nandya");
-insert into rgm values("jeil","rgm","django",287900,"nandya");
-insert into rgm values("fhhil","rgm","java",827900,"nandya");
-insert into rgm values("hfbue","rgm","pyhton",837900,"nandya");
-insert into rgm values("euwi","rgm","sql",879300,"nandya");
+insert into rgm values("sahil","rgm","sql",687900,"ben");
+insert into rgm values("sahvhw","rgm","c",987900,"nan");
+insert into rgm values("jbjl","rgm","pyhton",787900,"kur");
+insert into rgm values("sahwfyl","rgm","c++",487900,"che");
+insert into rgm values("fuewil","rgm","c",187900,"mum");
+insert into rgm values("jeil","rgm","django",287900,"kl");
+insert into rgm values("fhhil","rgm","java",827900,"nan");
+insert into rgm values("hfbue","rgm","pyhton",837900,"kur");
+insert into rgm values("euwi","rgm","sql",879300,"ben");
 select * from rgm;
 
 
