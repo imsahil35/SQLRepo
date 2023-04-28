c83ce5bc570e646253cafc10fa169a66a9b630d9
imsahil35 <130961969+imsahil35@users.noreply.github.com>
2023-04-28 09:29:34 +0530

    Add files via upload

---
+++ testq.sql
@@ -0,0 +1,20 @@
+-- create database HeroVired
+-- create table RGM ( 
+-- Name varchar(30), 
+-- College varchar(20), 
+-- Session varchar(20), 
+-- Contact_no int, 
+-- Address varchar(20)
+-- ); 
+-- insert into rgm values("sahil","rgm","ml",687900,"nandya");
+-- insert into rgm values("sahvhw","rgm","c",987900,"nandya");
+-- insert into rgm values("jbjl","rgm","pyhton",787900,"nandya");
+-- insert into rgm values("sahwfyl","rgm","c++",487900,"nandya");
+-- insert into rgm values("fuewil","rgm","c",187900,"nandya");
+-- insert into rgm values("jeil","rgm","django",287900,"nandya");
+-- insert into rgm values("fhhil","rgm","java",827900,"nandya");
+-- insert into rgm values("hfbue","rgm","pyhton",837900,"nandya");
+-- insert into rgm values("euwi","rgm","sql",879300,"nandya");
+select * from rgm;
+
+
